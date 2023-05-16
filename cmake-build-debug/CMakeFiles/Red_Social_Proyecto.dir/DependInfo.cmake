31836dd432bc09d43ad169ba10ab5358555d6614
Karencabello <karencabellogonzalez@gmail.com>
2023-05-16 09:12:33 +0200

    Merge branch 'main' of /Users/karen/Documents/GitHub/Red_Social_EDA_2 with conflicts.

---
+++ cmake-build-debug/CMakeFiles/Red_Social_Proyecto.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/karen/Documents/GitHub/Red_Social_EDA_2/main.c" "CMakeFiles/Red_Social_Proyecto.dir/main.c.o" "gcc" "CMakeFiles/Red_Social_Proyecto.dir/main.c.o.d"
+  "/Users/karen/Documents/GitHub/Red_Social_EDA_2/users.c" "CMakeFiles/Red_Social_Proyecto.dir/users.c.o" "gcc" "CMakeFiles/Red_Social_Proyecto.dir/users.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Red_Social_Proyecto.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Red_Social_Proyecto.dir/main.c.o"
+  "CMakeFiles/Red_Social_Proyecto.dir/main.c.o.d"
+  "CMakeFiles/Red_Social_Proyecto.dir/users.c.o"
+  "CMakeFiles/Red_Social_Proyecto.dir/users.c.o.d"
+  "Red_Social_Proyecto"
+  "Red_Social_Proyecto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Red_Social_Proyecto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
