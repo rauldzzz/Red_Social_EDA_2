c696916861f160f068b5875913d920ea2270c2b2
rauldzzz <raul.diaz01@estudiant.upf.edu>
2023-05-18 10:23:46 +0200

    actualizo amigos.h

--- amigos.h
+++ amigos.h
@@ -10,4 +10,18 @@ typedef struct{
     int top;
 }Stack;
 
+Stack* init_stack();
+
+int is_full(Stack* p, int N );
+
+int is_empty(Stack* p);
+
+int top(Stack* p);
+
+void push(Stack* p, int valor);
+
+void free_stack(Stack* p);
+
+
+
 #endif //RED_SOCIAL_PROYECTO_AMIGOS_H
